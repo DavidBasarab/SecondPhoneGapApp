a17d92626911b68901383cb58f2fcc26b885a994
David Basarab <dbasarab617@gmail.com>
2013-04-01 15:35:56 -0400

    Added the selection of the template to each grid.

--- js/main.js
+++ js/main.js
@@ -2,15 +2,31 @@ var app = {
 
     route: function () {
         var hash = window.location.hash;
-        var match = hash.ma(app.detailsURL);
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function (employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+            return;
+        }
+
+        $('body').html(new HomeView(this.store).render().el);
+    },
+
+    registerEvents: function () {
+        var self = this;
+
+        $(window).on('hashchange', $.proxy(this.route, this));
     },
 
     initialize: function () {
         var self = this;
         this.detailsURL = /^#employees\/(\d{1,})/;
         this.store = new MemoryStore(function () {
-            $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
+
+        this.registerEvents();
     }
 
 };
